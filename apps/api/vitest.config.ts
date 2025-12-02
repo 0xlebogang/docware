afbbb4edd3c3472236b40102537c5fd410b587e6
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-12-02 20:55:52 +0200

    chore: setup api testing lib

---
+++ apps/api/vitest.config.ts
@@ -0,0 +1,12 @@
+import { baseConfig } from "@repo/vitest-config";
+import { defineConfig, mergeConfig } from "vitest/config";
+
+export default mergeConfig(
+	baseConfig,
+	defineConfig({
+		test: {
+			name: "api",
+			environment: "node",
+		},
+	}),
+);
