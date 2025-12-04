dc2c67fa16141e12eb709829e465e07933442130
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-12-04 19:58:57 +0200

    feat: add auth app proxy logic

--- apps/auth/src/app/page.ts
+++ apps/auth/src/app/page.ts
@@ -1,23 +1,4 @@
-import { auth } from "@repo/better-auth/server";
-import { headers } from "next/headers";
-import { RedirectType, redirect } from "next/navigation";
-
-const DASHBOARD_URL = process.env.NEXT_PUBLIC_LANDING_DASHBOARD_URL;
-if (!DASHBOARD_URL) {
-	throw new Error(
-		"Missing 'NEXT_PUBLIC_LANDING_DASHBOARD_URL' environment variable",
-	);
-}
-
+// logic handled in proxy.ts
 export default async function Index() {
-	const session = await auth.api.getSession({
-		headers: await headers(),
-	});
-
-	// check if user is authenticated
-	if (!session) {
-		redirect("/sign-in", RedirectType.replace);
-	}
-
-	redirect(DASHBOARD_URL || "/dashboard", RedirectType.replace);
+	return;
 }
