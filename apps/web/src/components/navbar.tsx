9c30314fda09c626a710a4735b0a1a6b3d4b53af
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-12-06 07:43:30 +0200

    feat: render navbar user button on mobile
    - render the navbar user button on the navbar when the user is authenticated (on mobile)

--- apps/web/src/components/navbar.tsx
+++ apps/web/src/components/navbar.tsx
@@ -46,15 +46,22 @@ export function Navbar({
 								<Logo />
 							</Link>
 
-							<button
-								type="button"
-								onClick={() => setMenuState(!menuState)}
-								aria-label={menuState === true ? "Close Menu" : "Open Menu"}
-								className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
-							>
-								<Menu className="in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
-								<X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
-							</button>
+							{session?.session ? (
+								<UserButton
+									name={session.user.name}
+									imageUrl={session.user.image}
+								/>
+							) : (
+								<button
+									type="button"
+									onClick={() => setMenuState(!menuState)}
+									aria-label={menuState === true ? "Close Menu" : "Open Menu"}
+									className="relative z-20 -m-2.5 -mr-4 block cursor-pointer p-2.5 lg:hidden"
+								>
+									<Menu className="in-data-[state=active]:rotate-180 in-data-[state=active]:scale-0 in-data-[state=active]:opacity-0 m-auto size-6 duration-200" />
+									<X className="in-data-[state=active]:rotate-0 in-data-[state=active]:scale-100 in-data-[state=active]:opacity-100 absolute inset-0 m-auto size-6 -rotate-180 scale-0 opacity-0 duration-200" />
+								</button>
+							)}
 						</div>
 
 						<div className="bg-background in-data-[state=active]:block lg:in-data-[state=active]:flex mb-6 hidden w-full flex-wrap items-center justify-end space-y-8 rounded-3xl border p-6 shadow-2xl shadow-zinc-300/20 md:flex-nowrap lg:m-0 lg:flex lg:w-fit lg:gap-6 lg:space-y-0 lg:border-transparent lg:bg-transparent lg:p-0 lg:shadow-none dark:shadow-none dark:lg:bg-transparent">
