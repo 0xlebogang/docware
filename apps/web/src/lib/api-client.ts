f48dac2160db66a5417f31b7e3fb42418c84bc78
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-12-07 22:20:06 +0200

    chore: clean up client export

--- apps/web/src/lib/api-client.ts
+++ apps/web/src/lib/api-client.ts
@@ -1,4 +1,4 @@
 import type { ApiType } from "api/rpc";
 import { hc } from "hono/client";
 
-export const api = hc<ApiType>(process.env.NEXT_PUBLIC_API_URL as string);
+export const { api } = hc<ApiType>(process.env.NEXT_PUBLIC_API_URL as string);
