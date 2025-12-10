600520851ad514c24946e7bb7fc5a02a7fba2327
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-12-10 12:52:32 +0200

    feat(web): add auth based proxy logic

--- apps/web/src/proxy.ts
+++ apps/web/src/proxy.ts
@@ -1,25 +1,20 @@
 import { checkAuthStatus } from "@repo/auth/utils";
 import { headers } from "next/headers";
-import { type NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
 export const config = {
 	matcher: "/:path*",
 };
 
-export async function proxy(request: NextRequest) {
+export async function proxy() {
 	const headerList = await headers();
 	const isAuthenticated = await checkAuthStatus(headerList);
 
-	if (isAuthenticated) {
-		return NextResponse.redirect(
-			process.env.NEXT_PUBLIC_DASHBOARD_URL || "http://localhost:3000",
-		);
-	}
-
-	if (request.nextUrl.pathname === "/")
+	if (!isAuthenticated) {
 		return NextResponse.redirect(
 			process.env.NEXT_PUBLIC_SIGN_IN_URL || "http://localhost:3001/sign-in",
 		);
+	}
 
 	return NextResponse.next();
 }
--- packages/auth/src/utils.ts
+++ packages/auth/src/utils.ts
@@ -1,4 +1,4 @@
-import { auth } from "@repo/auth/server";
+import { auth } from "./server";
 
 export async function checkAuthStatus(headers: Headers) {
 	const session = await auth.api.getSession({
