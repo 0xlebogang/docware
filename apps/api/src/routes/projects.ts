95413d2b2e1fd1f2054394be04c5cc94a16443b1
Lebogang Phoshoko <phoshokolebogang@outlook.com>
2025-12-26 00:16:27 +0200

    feat(api): add project management routes

---
+++ apps/api/src/routes/projects.ts
@@ -0,0 +1,90 @@
+import { zValidator } from "@hono/zod-validator";
+import { db } from "@repo/database";
+import { ProjectInputSchema } from "@repo/database/validation";
+import { Hono } from "@repo/hono";
+import type { Env } from "@repo/hono/factory";
+import { storage } from "@/lib/storage-provider";
+import { ProjectService } from "@/services/projects.service";
+
+export type ProjectsEnv = Env & {
+	Variables: {
+		service: ProjectService;
+		organizationId: string;
+	};
+};
+
+const app = new Hono<ProjectsEnv>()
+	.use("*", async (c, next) => {
+		const service = new ProjectService(
+			db,
+			storage,
+			c.var.userID || "",
+			c.var.organizationId,
+		);
+		c.set("service", service);
+		await next();
+	})
+
+	.get("", async (c) => {
+		try {
+			const projects = await c.var.service.getAll();
+			return c.json({ data: projects }, 200);
+		} catch (error) {
+			console.error(error);
+			return c.json(
+				{ error: "An unexpected error occured. Please try again later." },
+				500,
+			);
+		}
+	})
+
+	.get("/:id", async (c) => {
+		const id = c.req.param("id");
+		try {
+			const project = await c.var.service.getByID(id);
+			if (!project) {
+				return c.json({ error: "Project not found." }, 404);
+			}
+
+			return c.json({ data: project }, 200);
+		} catch (error) {
+			console.error(error);
+			return c.json(
+				{ error: "An unexpected error occured. Please try again later." },
+				500,
+			);
+		}
+	})
+
+	.post("/create", zValidator("json", ProjectInputSchema), async (c) => {
+		try {
+			const input = c.req.valid("json");
+			const project = await c.var.service.create(input);
+			return c.json({ data: project }, 201);
+		} catch (error) {
+			console.error(error);
+			return c.json(
+				{ error: "An unexpected error occured. Please try again later." },
+				500,
+			);
+		}
+	})
+
+	.delete("/delete/:id", async (c) => {
+		const id = c.req.param("id");
+		try {
+			const success = await c.var.service.delete(id);
+			if (!success) {
+				return c.json({ error: "Project not found." }, 404);
+			}
+		} catch (error) {
+			console.error(error);
+			return c.json(
+				{ error: "An unexpected error occured. Please try again later." },
+				500,
+			);
+		}
+	});
+
+export default app;
+export type ProjectsRouteType = typeof app;
